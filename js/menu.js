44577aef5bac1c5e0d52dcd70ae6b076b0f03ee5
Chris Missal <chris.missal@gmail.com>
2013-02-01 23:27:48 -0600

    initial commit

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+chrome.contextMenus.create({
+  type: "normal",
+  title: "Wat Dat?!",
+  contexts: ["image"]
+}, function () {
+  var err = chrome.extension.lastError;
+  if (err) console.log(err);
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  chrome.tabs.create({'url': 'http://images.google.com/searchbyimage?image_url=' + encodeURIComponent(info.srcUrl) });
+});
