d9cb60c2f95a376039af73378d4ba5bb4a89f3da
Chris Missal <chris.missal@gmail.com>
2013-02-03 14:17:21 -0600

    added screenshots

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,12 @@
 chrome.contextMenus.create({
   type: "normal",
   title: "Wat Dat?!",
-  contexts: ["image"]
+  contexts: ["image", "link"]
 }, function () {
   var err = chrome.extension.lastError;
   if (err) console.log(err);
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  chrome.tabs.create({'url': 'http://images.google.com/searchbyimage?image_url=' + encodeURIComponent(info.srcUrl) });
+	chrome.tabs.create({'url': 'http://images.google.com/searchbyimage?image_url=' + encodeURIComponent(info.srcUrl) });
 });
